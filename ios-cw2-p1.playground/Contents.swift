7f34ac743f7ed07ef5d71892a92cea08bfed5422
kymalj <70738114+kymalj@users.noreply.github.com>
2020-09-12 21:36:59 -0700

    done

---
+++ ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,9 @@
+
+let family = ["tiba","aseel","modhaf", "khalefa", "aziz", "soud"]
+var evens = [2,4,6,8,10,12]
+var grades = [94.3,93.2,92.1]
+var trueFalse = [true, true, true, false]
+print(family[0],family[5])
+print(evens[0],evens[5])
+print(grades[0],grades[2])
+print(trueFalse[0],trueFalse[3])
---
+++ ios-cw2-p2.playground/Contents.swift
@@ -0,0 +1,15 @@
+
+
+var fruits = ["mango","mango","potato", "peach"]
+print(fruits[0])
+print(fruits[3])
+print(fruits)
+
+fruits.append("strwberry")
+print(fruits)
+
+fruits.remove(at:2)
+print(fruits)
+fruits += ["banana","watermelon","pineapple", "kiwi"]
+print(fruits)
+fruits.removeAll()
